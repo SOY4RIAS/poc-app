2e4bb6aed42c36cee9c03050b686514dcee8a710
Santiago Arias <sanx349@gmail.com>
2024-03-18 08:27:19 -0500

    fix: fix production build home page is not changing content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ interface HomeProps {
   };
 }
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home({ searchParams }: HomeProps) {
   const search = new URLSearchParams(searchParams);
 
